b61390435fbecc56d988d76912ea1d8fb1cd35a4
Fargetik <davidonsky1234@gmail.com>
2026-03-31 00:40:28 +0200

    Добавили SetUp и сделали тесты на моках

--- tests/StudentTest.php
+++ tests/StudentTest.php
@@ -6,12 +6,29 @@ use PHPUnit\Framework\TestCase;
 
 class StudentTest extends TestCase
 {
-   public function testAdd()
-   {
-       $student = new Student(null);
 
-       $result = $student->add("Ivan", 14, "Roman", True, "mouth_6");
+    protected function setUp(): void
+    {
+    $this->pdoMock = $this->createMock(PDO::class);
+    $this->student = new Student($this->pdoMock);
+    }
 
-       $this->assertEquals("Добавил пользователя", $result);
-   }
+    public function testAdd()
+    {
+
+        // $this->pdoMock = $this->createMock(PDO::class);
+        $mockStmt = $this->createMock(PDOStatement::class);
+        
+        $mockStmt->expects($this->once())
+                 ->method('execute')
+                 ->willReturn(true);
+        
+        $this->pdoMock->expects($this->once())
+               ->method('prepare')
+               ->willReturn($mockStmt);
+        
+        $this->student = new Student($this->pdoMock);
+        $result = $this->student->add("Ivan", 14, "Roman", true, "mouth_6");
+        
+    }
 }
